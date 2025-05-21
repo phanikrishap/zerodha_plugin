7d831a347c64b0056c4079b9045e483b7f0fdb60
Phani Krishna <phanikrishna.parvathaneni@gmail.com>
2025-05-21 11:07:05 +0530

    Initial commit - Full project backup including all files

--- QA.Tests/obj/Debug/net48/QA.Tests.AssemblyInfo.cs
+++ QA.Tests/obj/Debug/net48/QA.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QA.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec6f5326b9cfdab017e7d13eaaed4b7f253becd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36e00ccd0ab38b69e20197353c2f40fa71258ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("QA.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QA.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
